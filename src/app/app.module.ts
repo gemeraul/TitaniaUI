4b9e7fd4980fe25ae8e39baa30cfc2999e9c31c7
rgrimald <raul.grimaldi.barahona@intel.com>
2018-06-02 11:36:41 -0600

    Adding styling and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AngularFontAwesomeModule } from '../../node_modules/angular-font-awesome';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
