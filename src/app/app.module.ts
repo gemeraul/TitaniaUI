00f63d5cab36f3323b3db2163c697297ecd76e15
Raul Grimaldi <gemeraul94@gmail.com>
2018-06-07 18:48:55 -0600

    Adding routing to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
-
-
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,7 +22,8 @@ import { AngularFontAwesomeModule } from '../../node_modules/angular-font-awesom
     BrowserModule,
     AngularFontAwesomeModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
