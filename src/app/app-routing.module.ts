2ee708d69cfcd2fb52671dcb6b2a062a20ff9585
rgrimald <raul.grimaldi.barahona@intel.com>
2018-08-18 13:36:18 -0600

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { WebComponent } from './components/web/web.component';
-import { MobileComponent } from './components/mobile/mobile.component';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component'
 
 const routes: Routes = [
@@ -11,6 +10,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'web', component: WebComponent },
   { path: 'portfolio', component: ComingSoonComponent },
+  { path: 'ourteam', component: ComingSoonComponent },
+  { path: 'design', component: ComingSoonComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '/home' }
 ]
