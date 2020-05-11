f86b5c01034095a5677493e46ae35a6f09ff545a
Raul Grimaldi <gemeraul94@gmail.com>
2020-05-10 22:36:37 -0600

    Upgrading to ng9 finally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -17,15 +21,12 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { AngularFontAwesomeModule } from '../../node_modules/angular-font-awesome';
 import { ContactComponent } from './components/contact/contact.component';
 import { WebComponent } from './components/web/web.component';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
@@ -62,7 +63,6 @@ import { SocialMediaComponent } from './components/social-media/social-media.com
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
